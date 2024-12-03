beec7629c3349564107a5d51d0dd7f21d54ce9f9
janfiess <janfiess@users.noreply.github.com>
2024-12-03 16:03:56 +0100

    Send + Receive OSC msg (basic)

--- 08_Prototyping_Interfaces/E4_OSC_send_receive/E4_OSC_send_receive.ino
+++ 08_Prototyping_Interfaces/E4_OSC_send_receive/E4_OSC_send_receive.ino
@@ -1,6 +1,12 @@
-// Button signal to TouchDesigner and back via OSC
-// load library "OSC" by Adrian Freed
-// specify your Wifi ssid and pw, and IP address of your TouchDesigner PC
+/******************************************************************************************
+ * mc.ino
+ * Button signal to TouchDesigner and back via OSC
+ * LED leuchtet gelb, wenn der angeschlossene Hardware-Button am ESP32 gedrückt wird
+ * LED leuchtet blau, wenn der ESP32 OSC-Nachricht mit Topic "from_td" und Payload 0 empfängt
+ * Install library "OSC" by Adrian Freed
+ * specify your Wifi ssid and pw, and IP address of your receiver PC
+ ******************************************************************************************/
+
 
 #include <WiFi.h>
 #include <WiFiUdp.h>
@@ -8,15 +14,15 @@
 #include <OSCBundle.h>
 #include <OSCData.h>
 
-const char* ssid[] = {"tinkergarden", "Igloo"};   // @todo: add your wifi name
-const char* pass[] = {"strenggeheim", "1glooVision"};  // @todo: add your wifi pw
+const char* ssid = "tinkergarden";   // @todo: add your wifi name
+const char* pass = "strenggeheim";   // @todo: add your wifi pw
 
 WiFiUDP Udp;                                // A UDP instance to let us send and receive packets over UDP
-const IPAddress outIp(172, 20, 10, 8);      // @todo: add receiver IP address
-const unsigned int remotePort = 10000;      // This is the default port of the oscinChop in TouchDesigner
-const unsigned int localPort = 8888;        // Local port to listen for OSC packets (actually not used for sending)
+const IPAddress remoteIp(192, 168, 0, 20);  // @todo: add receiver IP address
+const unsigned int remotePort = 9000;      
+const unsigned int localPort = 8000;       
 
-const int buttonPin = 7;                   // GPIO13 for button input
+const int buttonPin = 7;                 
 int buttonState = 0;         
 int prev_buttonState = 0;
 const int led =  RGB_BUILTIN;                        
@@ -32,79 +38,70 @@ void setup() {
 }
 
 void connectWiFi() {
-  int num_networks = sizeof(ssid) / sizeof(ssid[0]);
-  for (int i = 0; i < num_networks; i++) {
-    Serial.print("Connecting to WiFi ");
-    Serial.println(ssid[i]);
-    WiFi.begin(ssid[i], pass[i]);
-    int attempts = 0;
-    while (WiFi.status() != WL_CONNECTED && attempts < 9) {
-      delay(1000);
-      Serial.print(".");
-      attempts++;
-    }   
-    Serial.printf("\nWiFi connected: SSID: %s, IP Address: %s\n", ssid, WiFi.localIP().toString().c_str());
+  WiFi.begin(ssid, pass);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
+  Serial.printf("WiFi connected: SSID: %s, IP Address: %s\n", ssid, WiFi.localIP().toString().c_str());
 }
 
 void connectUdp() {
-  Serial.println("Starting UDP");
   Udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(localPort);
+  Serial.println("Starting UDP - Local port: " + String(localPort));
 }
 
 void loop() {
-  sendToTD(); 
-  receiveFromTD();   
+  sendOSC(); 
+  receiveOSC();   
 }
 
-// Send button data to TouchDesigner
-void sendToTD() {
+void sendOSC() {
+  ////////////////// get button value
   buttonState = digitalRead(buttonPin);
   if(buttonState == prev_buttonState) return;
   prev_buttonState = buttonState;
 
-  // Check if the pushbutton is pressed. If it is, the buttonState is 1:
-  Serial.println("to TD: " + String(buttonState));
-  
-  if (buttonState == 1) rgbLedWrite(led, 255, 255, 0);  // gelb
-  else digitalWrite(led, 0);  // aus
+  ////////////////// feedback on serial port abd LED
+  if (buttonState == 1) rgbLedWrite(led, 255, 255, 0);  // LED gelb
+  else digitalWrite(led, 0);                            // LED aus
+  Serial.println("/to_td: " + String(buttonState));
 
-  OSCMessage msg("/to_td");
-  msg.add((int32_t)buttonState);   // Explicitly cast buttonState to int32_t
-  Udp.beginPacket(outIp, remotePort);
+  ////////////////// send value via OSC
+  OSCMessage msg("/to_td");        // define OSC key
+  msg.add((int32_t)buttonState);   // define OSC value
+  Udp.beginPacket(remoteIp, remotePort);
   msg.send(Udp);
   Udp.endPacket();
   msg.empty();
-  delay(500);
 }
 
-void receiveFromTD() {
+void receiveOSC() {
   OSCBundle bundle;
   int size = Udp.parsePacket();
-
   if (size > 0) {
     while (size--) {
       bundle.fill(Udp.read());
     }
+
     if (!bundle.hasError()) {
       for (int i = 0; i < bundle.size(); i++) {
         OSCMessage msg = bundle.getOSCMessage(i);
+
+        // Serial.println("Empfangen: " + String(msg.getAddress()));   // testen ob überhaupt irgendetwas ankommt
+
+        ////////////////// bestimmten OSC key empfangen und Aktion auslösen
         if (strcmp(msg.getAddress(), "/from_td") == 0) {
           int val = msg.getFloat(0);
-          if (val == 1) rgbLedWrite(led, 0, 0, 255);  // blau
-          else digitalWrite(led, 0);  // turn off
+          if (val == 1) rgbLedWrite(led, 0, 0, 255);  // LED blau
+          else digitalWrite(led, 0);  // LED off
 
-          Serial.print("/from_td: ");
-          Serial.println(val);
+          Serial.println("/from_td: " + String(val));
         }
       }
     } else {
-      OSCErrorCode error;
-      error = bundle.getError();
-      Serial.print("error: ");
-      Serial.println(error);
+      Serial.println("error: " + String(bundle.getError()));
     }
   }
 }
