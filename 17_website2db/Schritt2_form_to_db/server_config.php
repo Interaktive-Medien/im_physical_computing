40ab0d6fe91815baef55f642788c1a618fbcb056
janfiess <janfiess@users.noreply.github.com>
2025-03-19 23:56:57 +0100

    p

--- 17_website2db/Schritt2_form_to_db/server_config.php
+++ 17_website2db/Schritt2_form_to_db/server_config.php
@@ -2,13 +2,13 @@
 /*************************************************************
  * server_config.php
  * Datenbank-Verbindung
- * https://fiessling.ch/iot1/04_website2db/server_config.php
+ * https://fiessling.ch/im4/17_website2db/Schritt2_form_to_db/server_config.php
  *************************************************************/
 
 $db_host = "rv9w2f.myd.infomaniak.com";  // beim FHGR Edu-Server und xampp steht hier "localhost"
-$db_name = "rv9w2f_im4";  //  Edu-Server: "650665_4_1", xampp: "sensor2website"
-$db_user = "rv9w2f_fiessling";  //  Edu-Server: "650665_4_1", xampp: "root"
-$db_pass = "Strenggehe1m";      //  Edu-Server: "=rTjuEQDYvC9", xampp: ""
+$db_name = "rv9w2f_im4";                 // Edu-Server: "650665_4_1", xampp: "sensor2website"
+$db_user = "rv9w2f_fiessling";           // Edu-Server: "650665_4_1", xampp: "root"
+$db_pass = "Strenggehe1m";               // Edu-Server: "=rTjuEQDYvC9", xampp: ""
 
 $db_charset = "utf8";
 
